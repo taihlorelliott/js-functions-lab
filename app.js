9b8bf0f510439117ea66e1e96ffa4ae9e62ccb7c
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-10-18 10:54:16 -0400

    finished lab + level up

--- app.js
+++ app.js
@@ -245,3 +245,17 @@ function createUsername(firstName, lastName){
 
 console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
 
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+function numArgs(){
+    return arguments.length 
+}
+
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
